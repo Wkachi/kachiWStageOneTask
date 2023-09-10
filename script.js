12f803807ef3b8d7fd2fe9609d1fc9a789ff8d9a
Wkachi <123011868+Wkachi@users.noreply.github.com>
2023-09-10 15:30:02 +0000

    utc updated

--- script.js
+++ script.js
@@ -11,9 +11,15 @@ const timeSpace = document.querySelector('[data-testid="currentUTCTime"]');
 
 
 
-const timestamp = Date.now();
-console.log(`The current timestamp is ${timestamp}`);
+// const timestamp = Date.now();
+// console.log(`The current timestamp is ${timestamp}`);
 
- timeSpace.innerHTML = timestamp;
+//  timeSpace.innerHTML = timestamp;
 
-
+ let date = new Date("2023-09-10T16:17:41"); // local time
+ let utc = Date.UTC( date.getHours(), date.getMinutes(), date.getSeconds()); // UTC time in milliseconds
+ let iso = date.toISOString(); // UTC time in string format
+ console.log(utc); // 1697187461000
+ console.log(iso); // 2023-09-10T16:17:41.000Z
+ 
+ timeSpace.innerHTML = iso;
